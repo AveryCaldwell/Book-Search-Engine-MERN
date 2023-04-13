6cee84c77c36d7e7877b0131baa62c3f0dd3165d
Avery <averycaldwell7@gmail.com>
2023-04-13 15:54:46 -0700

    added Query and Mutation types

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,28 +1,44 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-  type Tech {
-    _id: ID!
-    name: String!
-  }
+    type Query {
+        me: [User]
+    }
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(input: BookInput!): User
+        removeBook(bookId: ID!): User
+    }
+    type User {
+        _id: ID!
+        username: String!
+        email: String!
+        bookCount: Int
+        savedBooks: [Book]
+    }
+    type Book {
+        bookId: ID!
+        authors: [String]
+        description: String
+        title: String!
+        image: String
+        link: String
+    }
 
-  type Matchup {
-    _id: ID!
-    tech1: String!
-    tech2: String!
-    tech1_votes: Int
-    tech2_votes: Int
-  }
+    input BookInput {
+        authors: [String]
+        description: String
+        title: String!
+        bookId: ID!
+        image: String
+        link: String
+    }
 
-  type Query {
-    tech: [Tech]
-    matchups(_id: String): [Matchup]
-  }
-
-  type Mutation {
-    createMatchup(tech1: String!, tech2: String!): Matchup
-    createVote(_id: String!, techNum: Int!): Matchup
-  }
+    type Auth {
+        token: ID!
+        user: User
+    }
 `;
 
 module.exports = typeDefs;

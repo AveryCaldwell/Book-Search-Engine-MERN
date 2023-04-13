eeb8128c0e3f2b6d371ac4d96fe32a12be41195e
Avery <averycaldwell7@gmail.com>
2023-04-13 16:03:02 -0700

    add queries.js & mutations.js; add apollo provider

--- client/src/App.js
+++ client/src/App.js
@@ -3,29 +3,31 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+    uri: '/graphql',
+    cache: new InMemoryCache(),
+});
 
 function App() {
-  return (
-    <Router>
-      <>
-        <Navbar />
-        <Routes>
-          <Route 
-            path='/' 
-            element={<SearchBooks />} 
-          />
-          <Route 
-            path='/saved' 
-            element={<SavedBooks />} 
-          />
-          <Route 
-            path='*'
-            element={<h1 className='display-2'>Wrong page!</h1>}
-          />
-        </Routes>
-      </>
-    </Router>
-  );
+    return (
+        <ApolloProvider client={client}>
+            <Router>
+                <>
+                    <Navbar />
+                    <Routes>
+                        <Route path='/' element={<SearchBooks />} />
+                        <Route path='/saved' element={<SavedBooks />} />
+                        <Route
+                            path='*'
+                            element={<h1 className='display-2'>Wrong page!</h1>}
+                        />
+                    </Routes>
+                </>
+            </Router>
+        </ApolloProvider>
+    );
 }
 
 export default App;

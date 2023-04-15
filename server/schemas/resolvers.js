1d95bbd1820d93dd1150f42d609b060a25319f70
Avery <averycaldwell7@gmail.com>
2023-04-15 13:12:11 -0700

    mutation set up on backend

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,40 +1,73 @@
-const { Tech, Matchup } = require('../models');
+const { User } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
 
-
+// This object defines the resolvers for GraphQL queries and mutations.
 const resolvers = {
     Query: {
-        tech: async () => {
-            return Tech.find({});
-        },
-        matchups: async (parent, { _id }) => {
-            const params = _id ? { _id } : {};
-            return Matchup.find(params);
+        me: async (parent, args, context) => {
+            // checks if a user is logged in and returns their data.
+            if (context.user) {
+                const userData = await User.findOne({
+                    _id: context.user._id,
+                }).select('-__v -password');
+                return userData;
+            }
+            throw new AuthenticationError('Not logged in');
         },
     },
+
     Mutation: {
-        createMatchup: async (parent, args) => {
-            const matchup = await Matchup.create(args);
-            return matchup;
+        //  logs in a user with their email and password.
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+            // checks if a user with the given email exists in the database.
+            if (!user) {
+                throw new AuthenticationError(
+                    'No user found with this email address'
+                );
+            }
+            // checks if the password provided matches the user's password in the database.
+            const correctPw = await user.isCorrectPassword(password);
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+            // generates a JWT for the logged-in user.
+            const token = signToken(user);
+            return { token, user };
         },
-        createVote: async (parent, { _id, techNum }) => {
-            const vote = await Matchup.findOneAndUpdate(
-                { _id },
-                { $inc: { [`tech${techNum}_votes`]: 1 } },
-                { new: true }
-            );
-            return vote;
+        // adds a new user to the database.
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            // generates a JWT for the new user.
+            const token = signToken(user);
+            return { token, user };
         },
-        updateMatchup: async (parent, { _id, tech1, tech2 }) => {
-            try {
-                const matchup = await Matchup.findByIdAndUpdate(
-                    _id,
-                    { tech1, tech2 },
+        //  saves a book to a user's list of saved books.
+        saveBook: async (parent, { input }, context) => {
+            // checks if a user is logged in and updates their saved books list.
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $addToSet: { savedBooks: input } },
+                    { new: true, runValidators: true }
+                );
+                return updatedUser;
+            }
+            throw new AuthenticationError('You must be loggin in, duh!');
+        },
+        //  removes a book from a user's list of saved books.
+        removeBook: async (parent, { bookId }, context) => {
+            // checks if a user is logged in and removes the specified book from their saved books list.
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { savedBooks: { bookId: bookId } } },
                     { new: true }
                 );
-                return matchup;
-            } catch (err) {
-                throw new Error('Failed to update matchup');
+                return updatedUser;
             }
+            throw new AuthenticationError('You must be loggin in, duh!');
         },
     },
 };

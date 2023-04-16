cf0db240f6a20bd9d147bdf854b599d6d03de343
Avery <averycaldwell7@gmail.com>
2023-04-16 15:49:27 -0700

    fixed middleware params

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -5,35 +5,33 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-  // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    authMiddleware: function ({ req }) {
+        // allows token to be sent via req.body, req.query, or headers
+        let token =
+            req.body.token || req.query.token || req.headers.authorization;
+        // We split the token string into an array and return actual token
 
-    // ["Bearer", "<tokenvalue>"]
-    if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
-    }
+        if (req.headers.authorization) {
+            token = token.split(' ').pop().trim();
+        }
 
-    if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
-    }
+        if (!token) {
+            return req;
+        }
 
-    // verify token and get user data out of it
-    try {
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = data;
-    } catch {
-      console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
-    }
+        // if token can be verified, add the decoded user's data to the request so it can be accessed in the resolver
+        try {
+            const { data } = jwt.verify(token, secret, { maxAge: expiration });
+            req.user = data;
+        } catch {
+            console.log('Invalid token');
+        }
 
-    // send to next endpoint
-    next();
-  },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
-
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-  },
+        // return the request object so it can be passed to the resolver as `context`
+        return req;
+    },
+    signToken: function ({ username, email, _id }) {
+        const payload = { username, email, _id };
+        return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+    },
 };

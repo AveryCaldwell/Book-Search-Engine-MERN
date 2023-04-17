2ca213a1fca615c8b7ebd3c00eee2e000d89289b
Avery <averycaldwell7@gmail.com>
2023-04-17 09:59:42 -0700

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverSelectionTimeoutMS: 5000,
-    dbName: 'mydatabase',
-});
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mydatabase',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        serverSelectionTimeoutMS: 5000,
+        dbName: 'mydatabase',
+    }
+);
 
 module.exports = mongoose.connection;
